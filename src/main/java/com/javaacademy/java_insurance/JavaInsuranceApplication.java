da47f68c1ccc090eaa9dcdc0e893dddf61a23e3e
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-11-12 16:37:22 +0300

    init commit

---
+++ src/main/java/com/javaacademy/java_insurance/JavaInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.javaacademy.java_insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInsuranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInsuranceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javaacademy/java_insurance/JavaInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaacademy.java_insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
